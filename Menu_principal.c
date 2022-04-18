e9033c3385e04d5d811a2ca78e9c79df596cf528
Alopez-hermida <78850423+Alopez-hermida@users.noreply.github.com>
2022-04-18 12:23:30 +0200

    txt nombres y punt
    escribo el bucle y el fichero de el nombre y las puntuaciones

--- Menu_principal.c
+++ Menu_principal.c
@@ -3,13 +3,16 @@
 void Continuar();
 int main(){
 	int niveldif;
+	char apodo[10];
 	char aux;
 	FILE *f1;
 	FILE *f2;
 	f1 = fopen("normascaperoom.txt","r");
+	f2 = fopen("nombreypuntuaciones.txt","w");
 	do{
 	printf("Bienvenido a nuestro scaperoom (presione enter para continuar)\n");
 	Continuar();
+
 	printf("Para empezar necesitas saber que es un scaperoom (presione enter para continuar)\n");
 	if(f1 == NULL)
     {
@@ -24,8 +27,20 @@ int main(){
     fclose(f1);
 
 	Continuar();
-	printf("Escriba el numero de jugadores\n");
-	//se introducen los datos en el fichero f2
+
+
+	printf("Escriba el nombre del jugador (maximo 10 letras)\n");
+	if(f1 == NULL)
+    {
+        printf("No se pudo abrir el fichero\n");
+        exit(1);
+    }
+    fgets(apodo,10,stdin);
+    fprintf(f2,"%s",apodo);
+    fclose(f2);
+
+
+
 	printf("\nAhora, seleccione la dificultad en la que quieres jugar:\n");
 	printf("1) Facil\n2) Dificil\n");
 	scanf("%d",&niveldif);
