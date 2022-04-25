012741d8abe004587e869279e3dbf6ce07be2847
jesus marin delgado <79969611+jesuppedia@users.noreply.github.com>
2022-04-25 10:59:29 +0200

    update juegos de buscar
    he añadido la prueba de buscar entre las estanterias y entre las cajas

--- juegos.h
+++ juegos.h
@@ -1,14 +1,20 @@
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 void nivel_dificil();
 void nivel_facil();
+
 
 void wordle();
 void ahorcado();
 void tres_en_raya();
 void continuar();
 void trivia_facil ();
+void buscarcajas ();
+void buscarestanteria ();
 
 
 //FUNCIONES DEL AHORCADO
