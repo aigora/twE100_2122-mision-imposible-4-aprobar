184fb439912e3078f735e8ce268973b08d9d2517
Alopez-hermida <78850423+Alopez-hermida@users.noreply.github.com>
2022-05-03 16:44:48 +0200

    estructura
    he metido el nombre y la puntuacion como una estructura, para que sea mas comodo, la puntuacion esta definida para empezar en 0, y se imprime bien el ¡¡n el txt

--- Menu_principal.c
+++ Menu_principal.c
@@ -6,10 +6,17 @@ void continuar();
 void nivel_dificil();
 void nivel_facil();
 
+typedef struct
+{
+    char apodo[10];
+    int puntuacion;
+
+}jugador;
+
+
 int main(){
 	setlocale (LC_CTYPE,"spanish");
-	int niveldif;
-	char apodo[10];
+	jugador j1 = {"nombre",0};
 	char aux;
 	int eleccion;
 	FILE *f1;
@@ -36,14 +43,12 @@ int main(){
 	continuar();
 
 
-	printf("Escriba el nombre del jugador (maximo 10 letras)\n");
-	if(f1 == NULL)
-    {
-        printf("No se pudo abrir el fichero\n");
-        exit(1);
-    }
-    fgets(apodo,10,stdin);
-    fprintf(f2,"%s",apodo);
+	printf("E,scriba el nombre del jugador (maximo 10 letras)\n");
+	scanf("%s",&j1.apodo);
+
+
+    fprintf(f2,"%s",j1.apodo);
+    fprintf(f2,"%i",j1.puntuacion);
     fclose(f2);
 
 
